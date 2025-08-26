a2e354fb0c5e7b65764b6c250dce0e90b02b6e60
sivakumar <gsivadevops@gmail.com>
2025-08-26 13:32:23 +0530

    count loop changes

--- count-loop/r53.tf
+++ count-loop/r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
   count = 4
   zone_id = var.zone_id
-  name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.daws84s.site
+  name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.devopslearning.store
   type    = "A"
   ttl     = 1
   records = [aws_instance.roboshop[count.index].private_ip]
